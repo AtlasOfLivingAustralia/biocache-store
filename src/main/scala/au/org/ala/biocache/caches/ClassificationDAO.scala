6d5c27bb27575e6c25bce32b9093d9e4d7696bad
djtfmartin <djtfmartin@gmail.com>
2014-02-19 04:40:46 +0000

    added patches from r4273
    git-svn-id: http://ala-portal.googlecode.com/svn/branches/biocache-reshuffle/biocache-store@4299 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/biocache/caches/ClassificationDAO.scala
+++ src/main/scala/au/org/ala/biocache/caches/ClassificationDAO.scala
@@ -6,6 +6,27 @@ import au.org.ala.checklist.lucene.model.{NameSearchResult, MetricsResultDTO}
 import au.org.ala.data.model.LinnaeanRankClassification
 import au.org.ala.biocache.model.Classification
 
+import au.org.ala.data.model.LinnaeanRankClassification
+import au.org.ala.util.ReflectBean
+import au.org.ala.checklist.lucene.model.{NameSearchResult,MetricsResultDTO}
+import au.org.ala.checklist.lucene.{CBIndexSearch, HomonymException, SearchResultException}
+import scala.io.Source
+import org.slf4j.LoggerFactory
+import java.net.URLEncoder
+import java.util.zip.GZIPInputStream
+import java.io.BufferedReader
+import scala.xml.XML
+import java.io.InputStreamReader
+import java.net.URL
+import scalaj.http.Http
+import collection.{mutable, JavaConversions}
+import org.ala.layers.client.Client
+import collection.mutable.{ArrayBuffer, HashMap}
+import java.text.MessageFormat
+
+//import org.apache.zookeeper.ZooKeeper.States
+import scala.util.parsing.json.JSON
+
 /**
  * A DAO for accessing classification information in the cache. If the
  * value does not exist in the cache the name matching API is called.
@@ -38,14 +59,18 @@ object ClassificationDAO {
   /**
    * Uses a LRU cache
    */
-  def getByHashLRU(cl:Classification) : Option[MetricsResultDTO] = {
+  def getByHashLRU(cl:Classification, count:Int=0) : Option[MetricsResultDTO] = {
     //use the vernacular name to lookup if there if there is no scientific name or higher level classification
     //we don't really trust vernacular name matches thus only use as a last resort
-    val hash = { if(cl.vernacularName==null || cl.scientificName != null || cl.specificEpithet != null || cl.infraspecificEpithet != null
-            || cl.kingdom != null || cl.phylum != null || cl.classs != null || cl.order != null || cl.family !=null  || cl.genus!=null)
+    val hash = {
+      if(cl.vernacularName == null || cl.scientificName != null || cl.specificEpithet != null
+        || cl.infraspecificEpithet != null || cl.kingdom != null || cl.phylum != null
+        || cl.classs != null || cl.order != null || cl.family !=null  || cl.genus!=null){
         Array(cl.kingdom,cl.phylum,cl.classs,cl.order,cl.family,cl.genus,cl.species,cl.specificEpithet,
-            cl.subspecies,cl.infraspecificEpithet,cl.scientificName).reduceLeft(_+"|"+_)
-              else cl.vernacularName
+          cl.subspecies,cl.infraspecificEpithet,cl.scientificName).reduceLeft(_+"|"+_)
+      } else {
+        cl.vernacularName
+      }
     }
 
     if (cl.scientificName == null){
@@ -63,8 +88,8 @@ object ClassificationDAO {
 
     val cachedObject = lock.synchronized { lru.get(hash) }
 
-    if(cachedObject!=null){
-       cachedObject.asInstanceOf[Option[MetricsResultDTO]]
+    if(cachedObject != null){
+      cachedObject.asInstanceOf[Option[MetricsResultDTO]]
     } else {
       //search for a scientific name if values for the classification were provided otherwise search for a common name
       val idnsr = if(cl.taxonConceptID != null)  nameIndex.searchForRecordByLsid(cl.taxonConceptID) else null
@@ -72,9 +97,9 @@ object ClassificationDAO {
       var resultMetric = {
         try {
           if(idnsr != null){
-              val metric = new MetricsResultDTO
-              metric.setResult(idnsr)
-              metric
+            val metric = new MetricsResultDTO
+            metric.setResult(idnsr)
+            metric
           }
           //if (cl.taxonConceptID != null) nameIndex.searchForRecordByLsid(cl.taxonConceptID)
           else if(hash.contains("|")) nameIndex.searchForRecordMetrics(new LinnaeanRankClassification(
@@ -109,41 +134,45 @@ object ClassificationDAO {
 
       if(resultMetric != null && resultMetric.getResult() != null){
 
-          //handle the case where the species is a synonym this is a temporary fix should probably go in ala-name-matching
-          var result:Option[MetricsResultDTO] = if(resultMetric.getResult.isSynonym){
-                val ansr =nameIndex.searchForRecordByLsid(resultMetric.getResult.getAcceptedLsid)
-                if(ansr != null){
-                   //change the name match metric for a synonym
-                  ansr.setMatchType(resultMetric.getResult.getMatchType())
-                  resultMetric.setResult(ansr)
-                  Some(resultMetric)
-                } else{
-                  None
-                }
-              } else Some(resultMetric)
-          //}
-          if(result.isDefined){
-              //change the name match metric for a synonym
-//              if(nsr.isSynonym())
-//                result.get.setMatchType(nsr.getMatchType())
-              //update the subspecies or below value if necessary
-              val rank = result.get.getResult.getRank
-              if(rank != null && rank.getId() >7000 && rank.getId <9999){
-                result.get.getResult.getRankClassification.setSubspecies(result.get.getResult.getRankClassification.getScientificName())
-              }
-          } else {
-            logger.debug("Unable to locate accepted concept for synonym " + resultMetric.getResult + ". Attempting a higher level match")
-            if((cl.kingdom != null || cl.phylum != null || cl.classs != null || cl.order != null || cl.family != null || cl.genus != null) && (cl.getScientificName() != null || cl.getSpecies() != null || cl.getSpecificEpithet() != null || cl.getInfraspecificEpithet() != null)){
-                val newcl = cl.clone()
-                newcl.setScientificName(null)
-                newcl.setInfraspecificEpithet(null)
-                newcl.setSpecificEpithet(null)
-                newcl.setSpecies(null)
-                updateClassificationRemovingMissingSynonym(newcl, resultMetric.getResult())
-                result = getByHashLRU(newcl)
+        //handle the case where the species is a synonym this is a temporary fix should probably go in ala-name-matching
+        var result:Option[MetricsResultDTO] = if(resultMetric.getResult.isSynonym){
+          val ansr =nameIndex.searchForRecordByLsid(resultMetric.getResult.getAcceptedLsid)
+          if(ansr != null){
+            //change the name match metric for a synonym
+            ansr.setMatchType(resultMetric.getResult.getMatchType())
+            resultMetric.setResult(ansr)
+            Some(resultMetric)
+          } else{
+            None
+          }
+        } else {
+          Some(resultMetric)
+        }
+
+        if(result.isDefined){
+          //update the subspecies or below value if necessary
+          val rank = result.get.getResult.getRank
+          if(rank != null && rank.getId() > 7000 && rank.getId < 9999){
+            result.get.getResult.getRankClassification.setSubspecies(result.get.getResult.getRankClassification.getScientificName())
+          }
+        } else {
+          logger.debug("Unable to locate accepted concept for synonym " + resultMetric.getResult + ". Attempting a higher level match")
+          if((cl.kingdom != null || cl.phylum != null || cl.classs != null || cl.order != null || cl.family != null || cl.genus != null) && (cl.getScientificName() != null || cl.getSpecies() != null || cl.getSpecificEpithet() != null || cl.getInfraspecificEpithet() != null)){
+            val newcl = cl.clone()
+            newcl.setScientificName(null)
+            newcl.setInfraspecificEpithet(null)
+            newcl.setSpecificEpithet(null)
+            newcl.setSpecies(null)
+            updateClassificationRemovingMissingSynonym(newcl, resultMetric.getResult())
+            if(count < 4){
+              result = getByHashLRU(newcl, count+1)
             } else {
-              logger.warn("Recursively unable to locate a synonym for " + cl)
+              logger.warn("Potential recursive issue with " + cl.getKingdom()+" " + cl.getPhylum + " " + cl.getClasss + " " + cl.getOrder + " " + cl.getFamily)
             }
+            //[Processor Thread 7] 710000 >> Last key : dr695|BAS|SOMBASE/TOTAL BIOCONSTRUCTORS|74175, records per sec: 772.2008 recursive Stack overflow
+          } else {
+            logger.warn("Recursively unable to locate a synonym for " + cl)
+          }
         }
         lock.synchronized { lru.put(hash, result) }
         result
