00ccd60637ba13709c820bac3c7a90ae7b07c1a3
natasha.carter@csiro.au <natasha.carter@csiro.au>
2014-03-26 23:54:21 +0000

    Retrofit the trunk fixes to the branch
    git-svn-id: http://ala-portal.googlecode.com/svn/branches/biocache-reshuffle/biocache-store@4394 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/main/scala/au/org/ala/biocache/export/DwCACreator.scala
+++ src/main/scala/au/org/ala/biocache/export/DwCACreator.scala
@@ -10,6 +10,7 @@ import scala.util.parsing.json.JSON
 import org.slf4j.LoggerFactory
 import au.org.ala.biocache.Config
 import au.org.ala.biocache.util.OptionParser
+import util.matching.Regex
 
 /**
  * Companion object for the DwCACreator class.
@@ -41,12 +42,22 @@ object DwCACreator {
     }
   }
 
+  // pattern to extract a data resource uid from a filter query , because the label show i18n value
+  val dataResourcePattern ="(?:[\"]*)?(?:[a-z_]*_uid:\")([a-z0-9]*)(?:[\"]*)?".r
   def getDataResourceUids : Seq[String] = {
     val url = Config.biocacheServiceUrl + "/occurrences/search?q=*:*&facets=data_resource_uid&pageSize=0&flimit=10000"
     val jsonString = Source.fromURL(url).getLines.mkString
     val json = JSON.parseFull(jsonString).get.asInstanceOf[Map[String, String]]
     val results = json.get("facetResults").get.asInstanceOf[List[Map[String, String]]].head.get("fieldResult").get.asInstanceOf[List[Map[String, String]]]
-    results.map(facet => facet.get("label").get)
+    results.map(facet => {
+      val fq = facet.get("fq").get
+      parseFq(fq)
+    }).filterNot(_.equals("Unknown"))
+
+  }
+  def parseFq(fq: String): String = fq match {
+    case dataResourcePattern(dr) => dr
+    case _ => "Unknown"
   }
 }
 
