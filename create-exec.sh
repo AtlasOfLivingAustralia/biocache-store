d63dfee3da7a1e96885124b2f1827bf1a1918e0e
djtfmartin <djtfmartin@gmail.com>
2014-06-28 06:01:04 +0000

    jackson update
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-store@4544 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- create-exec.sh
+++ create-exec.sh
@@ -1,8 +1,7 @@
 mvn clean install -DskipTests=true
 cp biocache.sh target/biocache
-cat target/biocache-store-1.2-SNAPSHOT.jar >> target/biocache
+cat target/biocache-store-*.jar >> target/biocache
 chmod 777 target/biocache
 cd target
 tar zcvf ../biocache.tgz biocache lib
 cd ..
-
