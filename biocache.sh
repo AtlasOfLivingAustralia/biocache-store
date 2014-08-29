14a0d0b7cff7a7d3aa74cce600af5994ef00bfe0
Dave Martin <djtfmartin@gmail.com>
2014-08-29 13:36:06 +1000

    reduce exec memory alloc
    reduce exec memory alloc to 2g instead of 16g

--- biocache.sh
+++ biocache.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-exec java $BIOCACHE_OPTS -Dfile.encoding=UTF8 -Dactors.corePoolSize=8 -Dactors.maxPoolSize=16 -Dactors.minPoolSize=8 -Djava.util.Arrays.useLegacyMergeSort=true -Xmx16g -Xms16g  -jar $0 "$@"
+exec java $BIOCACHE_OPTS -Dfile.encoding=UTF8 -Dactors.corePoolSize=8 -Dactors.maxPoolSize=16 -Dactors.minPoolSize=8 -Djava.util.Arrays.useLegacyMergeSort=true -Xmx2g -Xms2g  -jar $0 "$@"
 
 
