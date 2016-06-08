a9570afacd551a9e9c07e33f3d640d8c59d0005b
Patricia Koh <patricia.koh@csiro.au>
2016-06-08 17:07:57 +1000

    https://github.com/AtlasOfLivingAustralia/data-management/issues/217 - Processed Identification Qualifier 1) Added indexing to Solr for processed identification qualifier 2) Remove hard coded confirmed and unconfirmed list. Read the list from identificationQualifiers file

--- src/main/scala/au/org/ala/biocache/processor/IdentificationQualifierProcessor.scala
+++ src/main/scala/au/org/ala/biocache/processor/IdentificationQualifierProcessor.scala
@@ -1,16 +1,24 @@
 package au.org.ala.biocache.processor
 
+import java.io.File
+import au.org.ala.biocache.Config
 import au.org.ala.biocache.model.{FullRecord, QualityAssertion}
 import org.apache.commons.lang.StringUtils
 import org.slf4j.LoggerFactory
-
 import scala.collection.mutable.ArrayBuffer
 /**
   * Created by koh032 on 1/06/2016.
   */
 class IdentificationQualifierProcessor extends Processor {
 
-  val logger = LoggerFactory.getLogger("Indentification Qualifier Processor")
+  val logger = LoggerFactory.getLogger(getName)
+
+  val Certain = "Certain"
+  val Uncertain = "Uncertain"
+  val NotRecognised = "Not recognised"
+  val NotProvided = "Not provided"
+
+  val (certainKeywordList, unCertainKeywordList) = readFromFile ("/identificationQualifiers.txt")
 
   def process(guid: String, raw: FullRecord, processed: FullRecord, lastProcessed: Option[FullRecord] = None): Array[QualityAssertion] = {
 
@@ -27,31 +35,81 @@ class IdentificationQualifierProcessor extends Processor {
 
     val assertions = new ArrayBuffer[QualityAssertion]
 
-    val CERTAIN_KEYWORD_LIST = "\\bcertain\\b|\\bconfident\\b|\\bconfirm(?:ed)?\\b|\\bpositive\\b|\\bverified\\b"
-    val CertainKeywordRegex = ("((?:.*?)?(?:" + CERTAIN_KEYWORD_LIST + ")(?:.*)?)").r
+    val CertainKeywordRegex = ("((?:.*?)?(?:" + certainKeywordList + ")(?:.*)?)").r
 
-    val UNCERTAIN_KEYWORD_LIST = "\\bcf\\b[.]|\\baff\\b[.]|\\?|\\bunknown\\b|\\bnot certain\\b|\\bnegative\\b|\\buncertain\\b|" +
-      "\\buncertainty\\b|\\bincorrect\\b|\\bpossible\\b|\\bprobable\\b|\\bsp\\b[.]|\\bsp\\b|\\bunnamed\\b|" +
-      "\\bunsure\\b|\\bforsan\\b|\\bnear\\b|\\bx\\b|\\bnot sure\\b|\\bnot confirm(?:ed)?\\b|\\bnot correct\\b".r
-    val UncertainKeywordRegex = ("((?:.*?)?(?:" + UNCERTAIN_KEYWORD_LIST + ")(?:.*)?)").r
+    val UncertainKeywordRegex = ("((?:.*?)?(?:" + unCertainKeywordList + ")(?:.*)?)").r
 
     val stringVal = rawIdentificationQualifier
 
     // default value
-    var translatedIdQ = "Not provided"
+    var translatedIdQualifier = NotProvided
 
-    logger.debug ("Processing raw.identification.identificationQualifier: " + stringVal)
+    logger.debug("Processing raw.identification.identificationQualifier: " + stringVal)
 
     if (StringUtils.isNotBlank(stringVal)) {
-      stringVal toLowerCase match {
-        case UncertainKeywordRegex (keyword)  => logger.debug ("Identification Qualifier '" + stringVal + "' is mapped to Uncertain"); translatedIdQ = "Uncertain"
-        case CertainKeywordRegex (keyword) => logger.debug ("Identification Qualifier '" + stringVal + "' is mapped to Certain"); translatedIdQ = "Certain"
-        case _ => logger.debug ("Identification Qualifier '" + stringVal + "' is mapped to Not recognised"); translatedIdQ = "Not recognised"
+      stringVal.trim.replaceAll("\\s+", " ") toLowerCase match {
+        case UncertainKeywordRegex (keyword)  => logger.debug("Identification Qualifier '" + stringVal + "' is mapped to " + Uncertain); translatedIdQualifier = Uncertain
+        case CertainKeywordRegex (keyword) => logger.debug("Identification Qualifier '" + stringVal + "' is mapped to " + Certain); translatedIdQualifier = Certain
+        case _ => logger.debug("Identification Qualifier '" + stringVal + "' is mapped to " + NotRecognised); translatedIdQualifier = NotRecognised
       }
+    } else {
+      logger.debug("No Identification Qualifier provided");
     }
 
-    return (translatedIdQ, assertions)
+    return (translatedIdQualifier, assertions)
   }
 
-  def getName = "identification"
+  def readFromFile (filePath: String): (String, String) = {
+
+    var certainList = ""
+    var uncertainList = ""
+
+    val CertainHeader = "[" + Certain.toLowerCase + "]"
+    val UncertainHeader = "[" + Uncertain.toLowerCase + "]"
+
+    var switch = ""
+
+    getSource(filePath).getLines.foreach({ line =>
+      logger.debug(line);
+      if (StringUtils.isNotBlank(line)) {
+        line.trim toLowerCase match {
+          case CertainHeader => switch = CertainHeader; logger.debug("Encoutered " + CertainHeader)
+          case UncertainHeader => switch = UncertainHeader; logger.debug("Encoutered " + UncertainHeader)
+          case _ => if (switch == CertainHeader) {
+            if (certainList != "") certainList = certainList.concat("|" + regex(line)) else certainList = certainList.concat(regex(line));
+            logger.debug("Adding to certain List ")
+          } else if (switch == UncertainHeader) {
+            if (uncertainList != "") uncertainList = uncertainList.concat("|" + regex(line)) else uncertainList = uncertainList.concat(regex(line));
+            logger.debug("Adding to Uncertain List ")
+          }
+        }
+      }
+    })
+
+    logger.debug("Printing certain List from IdentifcationQualifier.txt : " + certainList)
+    logger.debug("Printing uncertain List from IdentifcationQualifier.txt : " + uncertainList)
+
+    return (certainList, uncertainList);
+  }
+
+  def regex(keyword: String): String = {
+    return "\\b" + keyword.replace(".", "[.]").replace("(ed)", "(?:ed)?").replaceAll("\\s+", " ") + "\\b"
+  }
+
+  def getSource (filePath: String) : scala.io.Source = {
+    val overrideFile = new File(Config.vocabDirectory + filePath)
+
+    if(overrideFile.exists){
+      //if external file exists, use this
+      logger.debug("Reading identificationQualifier file: " + overrideFile.getAbsolutePath)
+      scala.io.Source.fromFile(overrideFile, "utf-8")
+    } else {
+      //else use the file shipped with jar
+      logger.debug("Reading internal identificationQualifier file: " + filePath)
+      scala.io.Source.fromURL(getClass.getResource(filePath), "utf-8")
+    }
+
+  }
+
+  def getName = "IdentificationQualifierProcessor"
 }
