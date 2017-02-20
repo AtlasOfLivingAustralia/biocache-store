8c2dedb2ca92464c91376df4b8e9b6b3ebab61bd
Dave Martin <djtfmartin@gmail.com>
2017-02-20 15:50:10 +0000

    extra options to include  or exclude resources

--- src/main/scala/au/org/ala/biocache/tool/ProcessLocalRecords.scala
+++ src/main/scala/au/org/ala/biocache/tool/ProcessLocalRecords.scala
@@ -15,11 +15,20 @@ object ProcessLocalRecords extends Tool {
   def main(args:Array[String]){
 
     var threads:Int = 1
+    var drs:Seq[String] = List()
+    var skipDrs:Seq[String] = List()
+
     val parser = new OptionParser(help) {
       intOpt("t", "thread", "The number of threads to use", {v:Int => threads = v } )
+      opt("dr", "data-resource-list", "comma separated list of drs to process", {
+        v: String => drs = v.trim.split(",").map {_.trim}
+      })
+      opt("edr", "skip-data-resource-list", "comma separated list of drs to NOT process", {
+        v: String => skipDrs = v.trim.split(",").map {_.trim}
+      })
     }
     if(parser.parse(args)){
-      new ProcessLocalRecords().processRecords(threads)
+      new ProcessLocalRecords().processRecords(threads, drs, skipDrs)
     }
   }
 }
@@ -31,7 +40,7 @@ class ProcessLocalRecords {
 
   val logger = LoggerFactory.getLogger("ProcessLocalRecords")
 
-  def processRecords(threads:Int) : Unit = {
+  def processRecords(threads:Int, drs:Seq[String], skipDrs:Seq[String]) : Unit = {
 
     val processor = new RecordProcessor
     val start = System.currentTimeMillis()
@@ -40,19 +49,24 @@ class ProcessLocalRecords {
     //note this update count isnt threadsafe, so its inaccurate
     //its been left in to give a general idea of performance
     var updateCount = 0
+    var readCount = 0
 
     val total = Config.occurrenceDAO.pageOverRawProcessedLocal(record => {
       if(!record.isEmpty){
         val raw = record.get._1
         val rowkey = raw.rowKey
-        val (processed, assertions) = processor.processRecord(raw)
-        Config.occurrenceDAO.updateOccurrence(raw.rowKey, processed, Versions.PROCESSED)
-        updateCount += 1
+        readCount += 1
+        if((drs.isEmpty || drs.contains(raw.attribution.dataResourceUid)) &&
+          !skipDrs.contains(raw.attribution.dataResourceUid)){
+          val (processed, assertions) = processor.processRecord(raw)
+          Config.occurrenceDAO.updateOccurrence(raw.rowKey, processed, Versions.PROCESSED)
+          updateCount += 1
+        }
         if(updateCount % 1000 == 0){
           val end = System.currentTimeMillis()
           val timeInSecs = ((end-lastLog).toFloat / 1000f  )
           val recordsPerSec = Math.round(1000f/timeInSecs)
-          logger.info(s"Total processed : $updateCount. Last rowkey: $rowkey  Last 1000 in $timeInSecs seconds ($recordsPerSec records a second)")
+          logger.info(s"Total processed : $updateCount, total read: $readCount Last rowkey: $rowkey  Last 1000 in $timeInSecs seconds ($recordsPerSec records a second)")
           lastLog = end
         }
       }
