549847c17c3daabac3afc395896a4d8fda1ae7e8
Dave Martin <djtfmartin@gmail.com>
2017-02-20 15:50:40 +0000

    fixes for attribution update

--- src/main/scala/au/org/ala/biocache/tool/UpdateAttribution.scala
+++ src/main/scala/au/org/ala/biocache/tool/UpdateAttribution.scala
@@ -1,5 +1,7 @@
 package au.org.ala.biocache.tool
 
+import java.io.FileWriter
+
 import au.org.ala.biocache.Config
 import au.org.ala.biocache.cmd.Tool
 import au.org.ala.biocache.model.{Versions, FullRecord}
@@ -13,16 +15,32 @@ import org.slf4j.LoggerFactory
 object UpdateAttribution extends Tool {
 
   def cmd = "update-attribution"
-  def desc = "Update the attribution for the records on a node. This is fastest than full reprocess."
+  def desc = "Update the attribution for the records on a node. This is faster than full reprocess."
 
   def main(args:Array[String]){
 
     var threads:Int = 1
+    var uuid:String = null
+    var updateLog:FileWriter = null
+    var skippedLog:FileWriter = null
     val parser = new OptionParser(help) {
       intOpt("t", "thread", "The number of threads to use", {v:Int => threads = v } )
+      opt("uuid", "uuid", "Just update a single record", {
+        v: String => uuid = v
+      })
+      opt("ul", "update-log-file", "Update log file", {
+        v: String => updateLog = new FileWriter(v)
+      })
+      opt("sl", "skipped-log-file", "Skipped log file", {
+        v: String => skippedLog = new FileWriter(v)
+      })
     }
     if(parser.parse(args)){
-      new UpdateAttribution().updateRecords(threads)
+      if(uuid != null){
+        new UpdateAttribution().updateSingleRecord(uuid)
+      } else {
+        new UpdateAttribution().updateRecords(threads, updateLog, skippedLog)
+      }
     }
   }
 }
@@ -34,42 +52,75 @@ class UpdateAttribution {
 
   val logger = LoggerFactory.getLogger("UpdateAttribution")
 
-  def updateRecords(threads:Int) : Unit = {
+  val ap = new AttributionProcessor
 
-    val start = System.currentTimeMillis()
-    val ap = new AttributionProcessor
-
-    Config.occurrenceDAO.pageOverRawProcessedLocal({ rawProcess =>
-
-      if(!rawProcess.isEmpty){
-
-        val raw = rawProcess.get._1
-        val lastProcessed = rawProcess.get._2
-        val oldAttribution = lastProcessed.attribution.toMap
-
-        ap.process(raw.rowKey, raw, lastProcessed, Some(lastProcessed))
-
-        val newAttribution = lastProcessed.attribution.toMap
-
-        //compare the maps
-        var updateRequired = false
-        oldAttribution.keySet.foreach { key =>
-          if(oldAttribution.getOrElse(key, "") != newAttribution.getOrElse(key, "")){
-            updateRequired = true
-          }
-        }
-
-        if(updateRequired){
-          val mapToPersist = lastProcessed.attribution.toMap.map { case (k, v) => (k + Config.persistenceManager.fieldDelimiter + "p" -> v) }
-          //look for changes
-          Config.persistenceManager.put(raw.rowKey, "occ", mapToPersist, true, true)
-        }
-      }
-
-      true
-    }, threads)
-
-    val end = System.currentTimeMillis()
+  def updateRecords(threads:Int, updateLog:FileWriter, skippedLog:FileWriter) : Unit = {
+    val curried = updateAttribution(updateLog, skippedLog, debug=false) _
+    Config.occurrenceDAO.pageOverRawProcessedLocal(curried, threads)
+    if(updateLog != null) {
+      updateLog.flush()
+      updateLog.close()
+    }
+    if(skippedLog != null) {
+      skippedLog.flush()
+      skippedLog.close()
+    }
     logger.info(s"Update complete")
   }
+
+  def updateSingleRecord(uuid:String): Unit = {
+    val rawProcessed = Config.occurrenceDAO.getRawProcessedByRowKey(uuid)
+    if(!rawProcessed.isEmpty){
+      val raw = rawProcessed.get(0)
+      val processed = rawProcessed.get(1)
+      updateAttribution(null, null, debug=true)(Some((raw, processed)))
+    } else {
+      logger.warn(s"$uuid not recognised.")
+    }
+  }
+
+  def updateAttribution(updateLog:FileWriter, skippedLog:FileWriter, debug:Boolean = false)(rawProcess:Option[(FullRecord, FullRecord)]) : Boolean = {
+    if(!rawProcess.isEmpty){
+
+      val raw = rawProcess.get._1
+      val lastProcessed = rawProcess.get._2
+      val oldAttribution = lastProcessed.attribution.toMap
+
+      ap.process(raw.rowKey, raw, lastProcessed, Some(lastProcessed))
+
+      val newAttribution = lastProcessed.attribution.toMap
+
+      //compare the maps
+      var updateRequired = false
+      newAttribution.keySet.foreach { key =>
+        val oldValue = oldAttribution.getOrElse(key, "")
+        val newValue = newAttribution.getOrElse(key, "")
+        val diff = oldValue != newValue
+        if(debug){
+          println(s"$key old: $oldValue new: $newValue diff: $diff")
+        }
+        if(diff){
+          updateRequired = true
+        }
+      }
+      if(debug) {
+        println(s"Update required: $updateRequired")
+      }
+
+      if(updateRequired){
+        val mapToPersist = lastProcessed.attribution.toMap.map { case (k, v) =>
+          (k + Config.persistenceManager.fieldDelimiter + "p" -> v)
+        }
+        //look for changes
+        Config.persistenceManager.put(raw.rowKey, "occ", mapToPersist, true, true)
+        if(updateLog != null){
+          synchronized { updateLog.write(raw.rowKey + "\n") }
+        }
+      } else if(skippedLog != null){
+        synchronized { skippedLog.write(raw.rowKey + "\n") }
+      }
+    }
+    true
+  }
+
 }
