c0a331b0976730b3529d1a88182de6becdbb988f
Peter Ansell <p_ansell@yahoo.com>
2017-05-09 09:02:23 +1000

    Fix memory override parameters for appassembler
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- create-production-exec.sh
+++ create-production-exec.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mvn clean install -DskipTests=true -DinitialMemorySize=12g -DmaximumMemorySize=12g
+mvn clean install -DskipTests=true -DinitialMemorySize=16g -DmaxMemorySize=16g
 cp target/biocache-store-*-distribution.zip ./
