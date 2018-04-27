f63488b293a7fe3682184fb897cfc3b954a4985b
Dave Martin <djtfmartin@gmail.com>
2018-04-27 17:04:40 +0100

    removed broken support for cassandraFilterFile

--- src/main/scala/au/org/ala/biocache/export/ExportFromIndexStream.scala
+++ src/main/scala/au/org/ala/biocache/export/ExportFromIndexStream.scala
@@ -30,7 +30,7 @@ object ExportFromIndexStream extends Tool with Counter {
 
   def cmd = "export-stream"
 
-  def desc = "Export from search indexes using streaming."
+  def desc = "Export from search indexes using SOLR streaming."
 
   override val logger = LoggerFactory.getLogger("ExportFromIndexStream")
 
@@ -40,7 +40,6 @@ object ExportFromIndexStream extends Tool with Counter {
   var fieldsToExport = Array[String]()
   var orderFields = Array("row_key")
   var queryAssertions = false
-  var cassandraFilterFile = ""
 
   var keys: Option[Array[String]] = None
   var dr: Option[String] = None
@@ -67,17 +66,6 @@ object ExportFromIndexStream extends Tool with Counter {
       opt("qa", "quality-assertions", "Include query assertions", {
         queryAssertions = true
       })
-      opt("f", "filter-file", "Cassandra filter file for export-stream. File is a json file with an array of maps with " +
-        "the configuration of each export file. list-of-fields must contain all fields in -subset1 and " +
-        "-subset2. 'cassandraField' can be 'species_group'. 'value' can be '*'. -subset1 and -subset2 can" +
-        " include layer (el and/or cl) and qa field names when qa:true and layers:true. output-file value must end with '.csv'. " +
-        " the optional -subset1 and -subset2 field lists will create output files with the name extensions -brief " +
-        "and -standard" +
-        "[{filter:\"cassandraField:value\", output-file:filepath, qa:false, " +
-        "layers:false, user-qa:false, list-of-fields:id,scientificName" + Config.persistenceManager.fieldDelimiter + "p, list-of-fields-subset1:id, " +
-        "list-of-fields-subset2:scientificName" + Config.persistenceManager.fieldDelimiter + "p metadata-q:\"species_group:Fishes\"}]", {
-        v: String => cassandraFilterFile = v
-      })
       opt("k", "keys", "A comma separated list of keys on which to perform the range threads. Prevents the need to query SOLR for the ranges. Only used with a filter-file. ", {
         v: String => keys = Some(v.split(","))
       })
@@ -90,15 +78,10 @@ object ExportFromIndexStream extends Tool with Counter {
     }
 
     if (parser.parse(args)) {
-      if (cassandraFilterFile.length > 0) {
-        exportFromConfig()
+      if (outputFilePath.length <= 0 || fieldsToExport.length <= 0) {
+        parser.showUsage
       } else {
-        //without -f, argOpts are mandatory
-        if (outputFilePath.length <= 0 || fieldsToExport.length <= 0) {
-          parser.showUsage
-        } else {
-          exportFromQuery()
-        }
+        exportFromQuery()
       }
     }
   }
@@ -153,242 +136,36 @@ object ExportFromIndexStream extends Tool with Counter {
     fileWriter.close
   }
 
-
-  def exportFromConfig() {
-    //init
-    exportFromConfigInit()
-
-    //run
-    exportFromConfigRun()
-
-    //finalize
-    exportFromConfigFinish()
-  }
-
   def getHeader(fields: (Array[String], Array[String], Array[Int], Boolean), json: List[Map[String, Object]]): Array[String] = {
+
     val fields2 = if (fields._2 != null) fields._2 else Array[String]()
     val fields3 = if (fields._3 != null) fields._3.collect { case i: Int => AssertionCodes.getByCode(i).get.name } else Array[String]()
     val fields4 = if (fields._4) Array[String]("user_assertions") else Array[String]()
 
     (fields._1 ++ fields2 ++ fields3 ++ fields4).collect { case f: String =>
       var h = f
-      json.foreach(m => if (m.getOrElse("downloadName", "") == f) h = m.getOrElse("dwcTerm", m.getOrElse("downloadDescription", m.getOrElse("description", m.getOrElse("downloadName", f.toString())))).toString)
+      json.foreach(m => if (m.getOrElse("downloadName", "") == f) {
+        h = m.getOrElse(
+          "dwcTerm",
+          m.getOrElse("downloadDescription",
+            m.getOrElse("description",
+             m.getOrElse("downloadName", f.toString()))
+          )
+        ).toString
+      })
       if (h == f) {
-        json.foreach(m => if (m.getOrElse("name", "") == f) h = m.getOrElse("dwcTerm", m.getOrElse("downloadDescription", m.getOrElse("description", m.getOrElse("downloadName", f.toString())))).toString)
+        json.foreach(m => if (m.getOrElse("name", "") == f) {
+          h = m.getOrElse(
+            "dwcTerm",
+            m.getOrElse("downloadDescription",
+              m.getOrElse("description",
+                m.getOrElse("downloadName", f.toString()))
+            )
+          ).toString
+        })
       }
       h
     }
-  }
-
-  def exportFromConfigInit() {
-    val jp = new JSONParser()
-    filters = jp.parse(FileUtils.readFileToString(new File(cassandraFilterFile))).asInstanceOf[JSONArray]
-    for (j <- 0 until numThreads) outputFiles += mutable.ArrayBuffer[CSVWriter]()
-
-    for (i <- 0 until filters.size()) {
-      val filter = filters.get(i).asInstanceOf[JSONObject]
-
-      val file = new File(filter.get("output-file").toString)
-      var fields = filter.get("list-of-fields").toString.split(",")
-      var fieldsBrief = filter.get("list-of-fields-subset1").toString.split(",")
-      var fieldsNormal = filter.get("list-of-fields-subset2").toString.split(",")
-      val q = filter.get("q").toString
-      val fieldValue = filter.get("filter").toString.split(":")
-
-      var layers: Array[String] = if (filter.get("layers").toString.equals("true")) new LayersStore(Config.layersServiceUrl).getFieldIds().asScala.toArray[String] else null
-      val qa: Array[Int] = if (filter.get("qa").toString.equals("true")) AssertionCodes.retrieveAll.collect { case e: ErrorCode => e.code }.toArray else null
-
-      //move fields to layers where applicable
-      val (actualFields, actualLayers) = fieldsToLayers(fields, layers)
-
-      fieldsList += ((actualFields, actualLayers, qa, filter.get("user-qa").toString.equals("true")))
-
-      if (!file.getParentFile().exists()) {
-        file.getParentFile().mkdirs()
-      }
-
-      val url = Config.biocacheServiceUrl + "/occurrences/search?facets=institution_uid&facets=collection_uid&facets=data_resource_uid&pageSize=0&flimit=1000&q=" + URLEncoder.encode("q", "UTF-8")
-      FileUtils.copyURLToFile(new URL(url), new File(file.getParent() + "/metadata.json"), 20000, 20000)
-      new LayersStore(Config.layersServiceUrl).getFieldIds().asScala.toArray
-      //create csv writers for all threads
-      for (j <- 0 until numThreads) {
-        val csv = new CSVWriter(new OutputStreamWriter(new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(file.getPath() + j)))), ',', '"', '\\')
-        if (i == 0) outputFiles += mutable.ArrayBuffer[CSVWriter]()
-        outputFiles(j) += csv
-      }
-
-      filterList += ((fieldValue(0), fieldValue(1)))
-    }
-  }
-
-  def exportFromConfigFinish() {
-    for (list <- outputFiles) {
-      for (csv <- list) {
-        csv.flush()
-        csv.close()
-      }
-    }
-
-    //join parts
-    for (i <- 0 until filters.size()) {
-      val filter = filters.get(i).asInstanceOf[JSONObject]
-      var fields1 = filter.get("list-of-fields-subset1").toString.split(",")
-      var fields2 = filter.get("list-of-fields-subset2").toString.split(",")
-
-      val file = new File(filter.get("output-file").toString.replace(".csv", ".zip"))
-      val out = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(file)))
-      out.putNextEntry(new ZipEntry(file.getName().replace(".zip", ".csv")))
-      val csv = new CSVWriter(new OutputStreamWriter(out))
-
-      logger.info("building: " + file.getPath)
-
-      //header
-      var json: List[Map[String, Object]] = JSON.parseFull(Source.fromURL(new URL(Config.biocacheServiceUrl + "/index/fields")).mkString).get.asInstanceOf[List[Map[String, Object]]]
-      val fieldsdb = new LayersStore(Config.layersServiceUrl).getFieldIdsAndDisplayNames().asScala
-      for (elem <- fieldsdb) {
-        val m: List[Map[String, Object]] = List(Map("id" -> elem._1, "name" -> elem._2)); json = json ++ m
-      }
-
-      val header = getHeader(fieldsList(i), json)
-      csv.writeNext(header)
-      csv.flush()
-
-      var columns1: Array[Int] = null
-      var values1: Array[String] = null
-      var out1: ZipOutputStream = null
-      var csv1: CSVWriter = null
-      if (fields1.length > 0 && fields1(0).length > 0) {
-        logger.info("also building: " + file.getPath().replace(".zip", "-brief.zip"))
-        out1 = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(new File(file.getPath().replace(".zip", "-brief.zip")))))
-        out1.putNextEntry(new ZipEntry(file.getName().replace(".zip", "-brief.csv")))
-        csv1 = new CSVWriter(new OutputStreamWriter(out1))
-        val (f, l) = fieldsToLayers(fields1, null)
-        val header1 = getHeader((f, l, null, false), json)
-        columns1 = header1.collect { case v: String =>
-          var i = 0
-          for (j <- 0 until header.length) {
-            if (header(j) == v) i = j
-          }
-          i
-        }
-        csv1.writeNext(header1)
-        values1 = new Array[String](header1.length)
-      }
-
-      var columns2: Array[Int] = null
-      var values2: Array[String] = null
-      var out2: ZipOutputStream = null
-      var csv2: CSVWriter = null
-      if (fields2.length > 0 && fields2(0).length > 0) {
-        logger.info("also building: " + file.getPath().replace(".zip", "-standard.zip"))
-        out2 = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(new File(file.getPath().replace(".zip", "-standard.zip")))))
-        out2.putNextEntry(new ZipEntry(file.getName().replace(".zip", "-standard.csv")))
-        csv2 = new CSVWriter(new OutputStreamWriter(out2))
-        val (f, l) = fieldsToLayers(fields2, null)
-        val header2 = getHeader((f, l, null, false), json)
-        columns2 = header2.collect { case v: String =>
-          var i = 0
-          for (j <- 0 until header.length) {
-            if (header(j) == v) i = j
-          }
-          i
-        }
-        csv2.writeNext(header2)
-        values2 = new Array[String](header2.length)
-      }
-
-      //for each thread add contents to csv
-      val srcFile = new File(filter.get("output-file").toString)
-      for (i <- 0 until numThreads) {
-        logger.info("merging: " + srcFile.getPath + i)
-
-        val part = new File(srcFile.getPath + i)
-
-        //open gzip and write to out
-        var gz = new GZIPInputStream(new BufferedInputStream(new FileInputStream(part)))
-        StreamUtils.copy(gz, out)
-        gz.close()
-
-        if (columns1 != null || columns2 != null) {
-          gz = new GZIPInputStream(new BufferedInputStream(new FileInputStream(part)))
-          val reader = new CSVReader(new InputStreamReader(gz))
-          var line: Array[String] = reader.readNext()
-          while (line != null) {
-            var pos1 = 0
-            if (columns1 != null) {
-              for (i <- 0 until columns1.length) {
-                values1(pos1) = if (line.length > columns1(i)) line(columns1(i)) else ""
-                pos1 = pos1 + 1
-              }
-              csv1.writeNext(values1)
-            }
-
-            var pos2 = 0
-            if (columns2 != null) {
-              for (i <- 0 until columns2.length) {
-                values2(pos2) = if (line.length > columns2(i)) line(columns2(i)) else ""
-                pos2 = pos2 + 1
-              }
-              csv2.writeNext(values2)
-            }
-
-            line = reader.readNext()
-          }
-        }
-        gz.close()
-        part.delete()
-      }
-
-      if (csv1 != null) {
-        csv1.flush()
-        out1.flush()
-        out1.closeEntry()
-        out1.close()
-      }
-
-      if (csv2 != null) {
-        csv2.flush()
-        out2.flush()
-        out2.closeEntry()
-        out2.close()
-      }
-
-      out.flush()
-      out.closeEntry()
-      out.close()
-    }
-  }
-
-  def exportFromConfigRun() {
-
-    //TODO: update this method for cassandra3/solr6
-
-//    val (query, startValue, endValue) = if (dr.isDefined) {
-//      ("data_resource_uid:" + dr.get, dr.get + "|", dr.get + "|~")
-//    } else {
-//      ("*:*", start, end)
-//    }
-//
-//    val ranges = if (keys.isEmpty) {
-//      calculateRanges(numThreads, query, startValue, endValue)
-//    } else {
-//      generateRanges(keys.get, startValue, endValue)
-//    }
-//
-//    var counter = 0
-//    val threads = new ArrayBuffer[Thread]
-//    ranges.foreach { case (startKey, endKey) =>
-//      logger.info("start: " + startKey + ", end key: " + endKey)
-//
-//      //      val t = new Thread(new BulkColumnExporter(this, counter, startKey, endKey, fieldsList, outputFiles(counter), filterList))
-//      //      t.start
-//      //      threads += t
-//
-//      counter += 1
-//    }
-//
-//    //wait for threads to complete and merge all indexes
-//    threads.foreach { thread => thread.join }
   }
 
   def fieldsToLayers(fields: Array[String], layers: Array[String]): (Array[String], Array[String]) = {
