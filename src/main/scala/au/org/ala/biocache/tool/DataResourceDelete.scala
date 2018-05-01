fd01a99abb17a014a7e320348043122b342e4ad6
Peter Ansell <p_ansell@yahoo.com>
2018-05-01 11:38:30 +1000

    Restore dellog delete safety
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- src/main/scala/au/org/ala/biocache/tool/DataResourceDelete.scala
+++ src/main/scala/au/org/ala/biocache/tool/DataResourceDelete.scala
@@ -42,9 +42,8 @@ class DataResourceDelete(dataResource:String) extends RecordDeletor {
         val endUuid = startUuid + "~"
 
         pm.pageOverSelect("occ", (guid,map)=>{
-            // FIXME: Turned off the "dellog" safety to enable rescuing on sandbox
-            // The dellog fake delete needs to be an option rather than hardcoded here
-            occurrenceDAO.delete(guid, false, false)
+            // TODO: The dellog false delete needs to be an option rather than hardcoded here
+            occurrenceDAO.delete(guid, false, true)
             count= count +1
             true
         }, startUuid, endUuid, 1000, "rowKey", "uuid")
